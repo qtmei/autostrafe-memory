20187a16be7b4bb9f0cf8e332362cc215d342b73
May <92554133+qtmei@users.noreply.github.com>
2025-08-08 18:57:40 -0500

    Update DllMain.cpp

--- DllMain.cpp
+++ DllMain.cpp
@@ -1,57 +1,51 @@
 #include <Windows.h>
 #include <thread>
-#include <iostream>
-
-using namespace std;
 
 void Autostrafe(HINSTANCE hinstDll)
 {
-	AllocConsole();
-	SetConsoleTitleA("Meiware Autostrafe");
-	freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+	srand(time(0));
 
-	cout << "© 2021 Meiware.net\nhold mouse5 to autostrafe" << endl;
-
-	DWORD client = (DWORD)GetModuleHandle("client.dll");
-	DWORD engine = (DWORD)GetModuleHandle("engine.dll");
-	DWORD dwForceLeft = 0x799708; //32-bit: 0x799708, 64-bit: 0xA33908
-	DWORD dwForceRight = 0x799714; //32-bit: 0x799714, 64-bit: 0xA33918
-	DWORD dwm_angAbsRotation = 0x4FC574; //32-bit: 0x4FC574, 64-bit: 0x6571FC
+	unsigned long long client = (unsigned long long)GetModuleHandle("client.dll");
+	unsigned long long engine = (unsigned long long)GetModuleHandle("engine.dll");
+	unsigned long long forceLeft = 0xA34908;
+	unsigned long long forceRight = 0xA34918;
+	unsigned long long m_angAbsRotation = 0x6571FC;
 	float oldYaw = 0.0f;
-	bool lastDir = false, runOnce = true; //lastDir false=left, true=right
+	bool lastDir = false, runOnce = true; //lastDir false = left, true = right
 
 	while (!GetAsyncKeyState(VK_END))
 	{
 		if (GetAsyncKeyState(VK_XBUTTON2))
 		{
-			float yaw = *(float*)(engine + dwm_angAbsRotation + 0x4); //base is pitch, + 0x4 is yaw because a float is 4 bytes
+			float yaw = *(float*)(engine + m_angAbsRotation + 0x4); //base is pitch, + 0x4 is yaw because a float is 4 bytes
+			int delay = rand() % 8 + 4;
 
 			if (yaw > oldYaw || yaw == oldYaw && !lastDir) //we want to ensure A or D is still being pressed when the mouse is not being moved
 			{
-				*(int*)(client + dwForceRight) = 0;
-				*(int*)(client + dwForceLeft) = 1;
+				Sleep(delay); //random delay to appear human
 
+				*(int*)(client + forceRight) = 0;
+				*(int*)(client + forceLeft) = 1;
 				lastDir = false;
 			}
 			else if (yaw < oldYaw || yaw == oldYaw && lastDir)
 			{
-				*(int*)(client + dwForceLeft) = 0;
-				*(int*)(client + dwForceRight) = 1;
+				Sleep(delay);
 
+				*(int*)(client + forceLeft) = 0;
+				*(int*)(client + forceRight) = 1;
 				lastDir = true;
 			}
 
 			oldYaw = yaw;
 			runOnce = true;
 		}
-		else
-			if (runOnce) //we release the keys when mouse5 is not held, but only once
-			{
-				*(int*)(client + dwForceLeft) = 0;
-				*(int*)(client + dwForceRight) = 0;
-
-				runOnce = false;
-			}
+		else if (runOnce) //we release the keys when mouse5 is not held, but only once
+		{
+			*(int*)(client + forceLeft) = 0;
+			*(int*)(client + forceRight) = 0;
+			runOnce = false;
+		}
 
 		Sleep(1);
 	}
